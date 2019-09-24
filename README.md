d6743ba06980547b7a8f9c05004bc396de9b5245
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2019-09-24 22:12:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Monogamy
+========
+
+Enjoy your favorite monospace font on all websites.
---
+++ script.js
@@ -0,0 +1,42 @@
+function isMono(node) {
+    switch (node.tagName) {
+    case "CODE": return true;
+    case "PRE": return true;
+    default: return false;
+    }
+}
+
+function traverse(node) {
+    if (!(node instanceof HTMLElement)) return;
+
+    const style = window.getComputedStyle(node, null);
+    const fontFamily = style.getPropertyValue('font-family');
+    const index = fontFamily.indexOf('monospace') > 0;
+    if (index > 0 || index < 0 && isMono(node)) {
+        node.style.setProperty('font-family', 'monospace', 'important');
+    }
+
+    for (let child of node.childNodes) {
+        traverse(child);
+    }
+}
+
+function fixStyleSheets() {
+    for (let css of document.styleSheets) {
+        for (let rule of css.cssRules) {
+            if (!rule.style || !rule.style.fontFamily) {
+                continue;
+            }
+            if (rule.style.fontFamily.indexOf('monospace') > 0) {
+                rule.style.fontFamily = 'monospace';
+            }
+        }
+    }
+}
+
+try {
+    fixStyleSheets();
+} catch (e) {
+    // ignore errors
+}
+traverse(document.body);
